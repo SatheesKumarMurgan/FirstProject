d69fd5ecf661f62a50412bb25b7f6f84dfdb3179
SatheesKumarMurugan <satheeskumar081@gmail.com>
2022-12-08 11:36:53 +0530

    new line from sathees

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -2,44 +2,47 @@ package org.emp;
 
 public class Employee {
 
-    private void empId() {
+	private void empId() {
 		System.out.println("empId.cr07");
 	}
-	
+
 	private void empNmae() {
-	    System.out.println("empName.loganathan");
-      
+		System.out.println("empName.loganathan");
+
 	}
+
 	private void empDob() {
 		System.out.println("empDob.9/11/1996");
 
 	}
-	
+
 	private void empPhn() {
 		System.out.println("7010036677");
 
 	}
+
 	private void empEmail() {
 		System.out.println("kumar@11gmail.com");
 
 	}
+
 	private void empAddress() {
-	    System.out.println("madurai");
+		System.out.println("madurai");
 
 	}
+
 	private void empPincode() {
 		System.out.println("600097");
 
 	}
-  public static void main(String[] args) {
-   Employee e = new Employee();
-   e.empAddress();
-}
-	
-}
-	
-	
-	
-	
-	
 
+	private void empSalary() {
+		System.out.println("900000");
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empAddress();
+	}
+
+}
